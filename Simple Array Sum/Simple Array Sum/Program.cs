5a717e1fb2a0d7e289a61cc52191b5a72f257adb
Unknown <theidproblem@yahoo.com>
2018-12-08 08:12:39 +0200

    Simple Array Sum

---
+++ Simple Array Sum/Simple Array Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Simple_Array_Sum
+{
+    class Program
+    {
+        /*
+         * Complete the simpleArraySum function below.
+         */
+        static int simpleArraySum(int[] ar)
+        {
+            return ar.Sum();
+
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+            ;
+            int result = simpleArraySum(ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
