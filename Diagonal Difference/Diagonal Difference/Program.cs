00c45fdaea0337ce5734958d86bd36284dceebcc
Unknown <theidproblem@yahoo.com>
2018-12-08 10:02:16 +0200

    Diagonal Difference

---
+++ Diagonal Difference/Diagonal Difference/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Diagonal_Difference
+{
+    class Program
+    {
+        // Complete the diagonalDifference function below.
+        static int diagonalDifference(int[][] arr)
+        {
+            int result = 0;
+            int it = arr.Length;
+
+            for (int i = 0; i < arr.Length; i++)  result += arr[i][--it] - arr[i][i];
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[][] arr = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            int result = diagonalDifference(arr);
+
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
